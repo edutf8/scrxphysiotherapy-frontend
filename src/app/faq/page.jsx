5e49ef7f60e34551419b7de64885c2cd919677e1
Ed Lewis <ed@edward-lewis.dev>
2024-08-20 14:37:23 +0100

    FAQ

---
+++ src/app/faq/page.jsx
@@ -0,0 +1,20 @@
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function FAQ() {
+    return (
+        <div className={"bg-white min-h-screen"}>
+            <section id={"navigation"}>
+                <Navbar/>
+                <Header/>
+            </section>
+            <section id={"team"}>
+
+            </section>
+            <section id={"footer"}>
+                <Footer />
+            </section>
+        </div>
+    )
+}
