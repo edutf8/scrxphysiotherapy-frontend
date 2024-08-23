3f91e511f7324f48a1f5c48ce1cf6defbac01437
Edward Lewis <e.lewis371@gmail.com>
2024-08-23 12:18:45 +0100

    Well that didn't work

--- src/app/components/TeamLayout.jsx
+++ src/app/components/TeamLayout.jsx
@@ -1,8 +1,4 @@
-"use client";
-import Image from "next/image";
-import React, { useEffect, useId, useRef, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import { useOutsideClick } from "../hooks/use-outside-click";
+import {useEffect, useState} from "react";
 
 const fetchData = async () => {
     try {
@@ -14,7 +10,10 @@ const fetchData = async () => {
         const data = await response.json();
         console.log('Fetched data:', data);
 
+        // Check if the data is an array, if not, convert it into an array
         const dataArray = Array.isArray(data) ? data : [data];
+
+        console.log('Data as array:', dataArray);
         return dataArray;
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -22,11 +21,8 @@ const fetchData = async () => {
     }
 };
 
-export function TeamLayout() {
+export default function TeamLayout() {
     const [teamData, setTeamData] = useState([]);
-    const [active, setActive] = useState(null);
-    const id = useId();
-    const ref = useRef(null);
 
     useEffect(() => {
         fetchData().then(data => {
@@ -34,133 +30,27 @@ export function TeamLayout() {
         });
     }, []);
 
-    useEffect(() => {
-        const onKeyDown = (event) => {
-            if (event.key === "Escape") {
-                setActive(null);
-            }
-        };
-
-        if (active) {
-            document.body.style.overflow = "hidden";
-            window.addEventListener("keydown", onKeyDown);
-        } else {
-            document.body.style.overflow = "auto";
-        }
-
-        return () => {
-            document.body.style.overflow = "auto";
-            window.removeEventListener("keydown", onKeyDown);
-        };
-    }, [active]);
-
-    useOutsideClick(ref, () => setActive(null));
-
-    if (!Array.isArray(teamData) || !teamData.length) return <div>Loading...</div>;
+    if (!Array.isArray(teamData) || !teamData.length) return <div>Loading...</div>; // Handle the loading state
 
     return (
-        <>
-            <AnimatePresence>
-                {active && typeof active === "object" && (
-                    <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                        className="fixed inset-0 bg-black/20 h-full w-full z-10"
+        <div className={"grid grid-cols-2 md:grid-cols-3 gap-8 md:gap-12"}>
+            {teamData.map((team, index) => (
+                <div key={index} className={"text-center"}>
+                    <img
+                        className={"rounded-xl sm:size-48 lg:size-60 mx-auto"}
+                        src={team.image} // Use the avatar URL from the data
+                        alt={`${team.name}'s Avatar`} // Dynamic alt text based on name
                     />
-                )}
-            </AnimatePresence>
-
-            <AnimatePresence>
-                {active && typeof active === "object" && (
-                    <div className="fixed inset-0 grid place-items-center z-[100]">
-                        <motion.div
-                            layoutId={`card-${active.name}-${id}`}
-                            ref={ref}
-                            className="w-full max-w-[500px] h-full md:h-fit md:max-h-[90%] flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
-                        >
-                            <motion.div layoutId={`image-${active.name}-${id}`}>
-                                <Image
-                                    layout={"responsive"}
-                                    width={1328}
-                                    height={768}
-                                    src={active.image}
-                                    alt={active.name}
-                                    className="rounded-lg object-cover object-top"
-                                    unoptimized
-                                />
-                            </motion.div>
-
-                            <div>
-                                <div className="flex justify-between items-start p-4">
-                                    <motion.h3
-                                        layoutId={`name-${active.name}-${id}`}
-                                        className="font-medium text-neutral-700 dark:text-neutral-200 text-base"
-                                    >
-                                        {active.name}
-                                    </motion.h3>
-                                    <motion.p
-                                        layoutId={`position-${active.position}-${id}`}
-                                        className="text-neutral-600 dark:text-neutral-400 text-base"
-                                    >
-                                        {active.position}
-                                    </motion.p>
-                                </div>
-                                <div className="pt-4 relative px-4">
-                                    <motion.div
-                                        layout
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        exit={{ opacity: 0 }}
-                                        className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
-                                    >
-                                        {active.description}
-                                    </motion.div>
-                                </div>
-                            </div>
-                        </motion.div>
+                    <div className={"mt-2 sm:mt-4"}>
+                        <h3 className={"text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-neutral-200"}>
+                            {team.name} {/* Dynamic name */}
+                        </h3>
+                        <p className={"text-xs text-gray-600 sm:text-sm lg:text-base dark:text-neutral-400"}>
+                            {team.position} {/* Dynamic position */}
+                        </p>
                     </div>
-                )}
-            </AnimatePresence>
-
-            <ul className="container mx-auto w-full grid grid-cols-1 md:grid-cols-3 items-start gap-4">
-                {teamData.map((team) => (
-                    <motion.div
-                        layoutId={`card-${team.name}-${id}`}
-                        key={team.name}
-                        onClick={() => setActive(team)}
-                        className="p-4 flex flex-col hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
-                    >
-                        <div className="bg-white shadow-lg rounded-lg overflow-hidden my-5 transition duration-300 transform hover:shadow-2xl hover:-translate-y-2 mb-5">
-                            <div className="flex gap-4 flex-col w-full">
-                                <motion.div layoutId={`image-${team.name}-${id}`}>
-                                    <Image
-                                        layout={"responsive"}
-                                        width={1328}
-                                        height={768}
-                                        src={team.image}
-                                        alt={team.name}
-                                        className="rounded-lg object-cover object-top"
-                                        unoptimized
-                                    />
-                                </motion.div>
-                                <div className="flex justify-center items-center flex-col">
-                                    <motion.h3
-                                        layoutId={`name-${team.name}-${id}`}
-                                        className="font-bold text-xl text-neutral-800 dark:text-neutral-200 text-center md:text-left"
-                                    >
-                                        {team.name}
-                                    </motion.h3>
-                                    <motion.p className="text-neutral-600 dark:text-neutral-400">
-                                        {team.position}
-                                    </motion.p>
-                                </div>
-                            </div>
-                            <div className="border-t-4 border-b-4 border-blue-500"></div>
-                        </div>
-                    </motion.div>
-                ))}
-            </ul>
-        </>
+                </div>
+            ))}
+        </div>
     );
 }
