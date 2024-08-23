5aca60ed22df783b4cbc09b547eb7b1a1d5be511
Edward Lewis <e.lewis371@gmail.com>
2024-08-23 12:22:19 +0100

    ffs

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import TeamLayout from "../components/TeamLayout";
 
 export default function Team() {
     return (
@@ -10,7 +11,9 @@ export default function Team() {
                 <Header/>
             </section>
             <section id={"team"}>
-
+                <div className={"container mx-auto"}>
+                    <TeamLayout />
+                </div>
             </section>
             <section id={"footer"}>
                 <Footer />
