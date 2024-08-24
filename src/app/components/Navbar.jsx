c9a78f25592ab9cd91334c74ea1353bc1796d163
Edward Lewis <e.lewis371@gmail.com>
2024-08-24 13:26:50 +0100

    Navbar update

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
     return (
         <nav className="bg-white shadow-lg p-5">
@@ -13,10 +14,24 @@ export default function Navbar() {
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
                         <div className="hidden md:flex space-x-8">
-                            <Link href="/services" className="text-gray-800 hover:text-gray-500">Services</Link>
-                            <Link href="/clinics" className="text-gray-800 hover:text-gray-500">Clinics</Link>
-                            <Link href="/team" className="text-gray-800 hover:text-gray-500">Team</Link>
-                            <Link href="/partners" className="text-gray-800 hover:text-gray-500">Partners</Link>
+                            <Link href="/services" className="text-gray-800 hover:text-blue-600 transition duration-300">Services</Link>
+                            <Link href="/clinics" className="text-gray-800 hover:text-blue-600 transition duration-300">Clinics</Link>
+                            <Link href="/team" className="text-gray-800 hover:text-blue-600 transition duration-300">Team</Link>
+                            <div className="relative">
+                                <button
+                                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                                    className="text-gray-800 hover:text-blue-600 transition duration-300"
+                                >
+                                    Partners
+                                </button>
+                                {isDropdownOpen && (
+                                    <div className="absolute left-0 mt-2 w-48 bg-white border border-gray-300 shadow-lg rounded-lg">
+                                        <Link href="/partners/partner1" className="block px-4 py-2 text-gray-800 hover:bg-blue-100">Partner 1</Link>
+                                        <Link href="/partners/partner2" className="block px-4 py-2 text-gray-800 hover:bg-blue-100">Partner 2</Link>
+                                        <Link href="/partners/partner3" className="block px-4 py-2 text-gray-800 hover:bg-blue-100">Partner 3</Link>
+                                    </div>
+                                )}
+                            </div>
                         </div>
                     </div>
 
@@ -24,22 +39,33 @@ export default function Navbar() {
                         <Link href="/">
                             <Image
                                 src={"/SCRx-logo.jpeg"}
-                                alt={"logo"} className={"w-72 md:w-48 lg:w-72"} width={300} height={500}/>
+                                alt={"logo"}
+                                className={"w-48 md:w-48 lg:w-72 mx-auto"}
+                                width={300} height={500}
+                            />
                         </Link>
                     </div>
 
                     <div className="flex items-center">
                         <div className="hidden md:flex space-x-8">
-                            <Link href="/education" className="text-gray-800 hover:text-gray-500">Education</Link>
-                            <Link href="/journey" className="text-gray-800 hover:text-gray-500">Journey</Link>
-                            <Link href="/vision" className="text-gray-800 hover:text-gray-500">Vision</Link>
-                            <Link href="/contact" className="text-gray-800 hover:text-gray-500">Contact Us</Link>
+                            <Link href="/education" className="text-gray-800 hover:text-blue-600 transition duration-300">Education</Link>
+                            <Link href="/journey" className="text-gray-800 hover:text-blue-600 transition duration-300">Journey</Link>
+                            <Link href="/vision" className="text-gray-800 hover:text-blue-600 transition duration-300">Vision</Link>
+                            <Link href="/contact" className="text-gray-800 hover:text-blue-600 transition duration-300">Contact Us</Link>
                         </div>
 
-                        <div className="md:hidden">
+                        <div className="md:hidden flex items-center">
+                            <Link href="/" className="mr-auto">
+                                <Image
+                                    src={"/SCRx-logo.jpeg"}
+                                    alt={"logo"}
+                                    className={"w-36"}
+                                    width={150} height={250}
+                                />
+                            </Link>
                             <button
                                 onClick={() => setIsOpen(!isOpen)}
-                                className="text-gray-800 hover:text-gray-500 focus:outline-none"
+                                className="text-gray-800 hover:text-blue-600 focus:outline-none ml-auto"
                             >
                                 <svg
                                     className="h-6 w-6"
@@ -73,12 +99,25 @@ export default function Navbar() {
             {isOpen && (
                 <div className="md:hidden">
                     <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 text-center">
-                        <Link href="/services" className="block text-gray-800 hover:text-gray-500">Services</Link>
-                        <Link href="/clinics" className="block text-gray-800 hover:text-gray-500">Clinics</Link>
-                        <Link href="/team" className="block text-gray-800 hover:text-gray-500">Team</Link>
-                        <Link href="/journey" className="block text-gray-800 hover:text-gray-500">Journey</Link>
-                        <Link href="/vision" className="block text-gray-800 hover:text-gray-500">Vision</Link>
-                        <Link href="/contact" className="block text-gray-800 hover:text-gray-500">Contact Us</Link>
+                        <Link href="/services" className="block text-gray-800 hover:text-blue-600">Services</Link>
+                        <Link href="/clinics" className="block text-gray-800 hover:text-blue-600">Clinics</Link>
+                        <Link href="/team" className="block text-gray-800 hover:text-blue-600">Team</Link>
+                        <button
+                            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                            className="block w-full text-gray-800 hover:text-blue-600"
+                        >
+                            Partners
+                        </button>
+                        {isDropdownOpen && (
+                            <div className="bg-white border border-gray-300 shadow-lg rounded-lg">
+                                <Link href="/partners/partner1" className="block px-4 py-2 text-gray-800 hover:bg-blue-100">Partner 1</Link>
+                                <Link href="/partners/partner2" className="block px-4 py-2 text-gray-800 hover:bg-blue-100">Partner 2</Link>
+                                <Link href="/partners/partner3" className="block px-4 py-2 text-gray-800 hover:bg-blue-100">Partner 3</Link>
+                            </div>
+                        )}
+                        <Link href="/journey" className="block text-gray-800 hover:text-blue-600">Journey</Link>
+                        <Link href="/vision" className="block text-gray-800 hover:text-blue-600">Vision</Link>
+                        <Link href="/contact" className="block text-gray-800 hover:text-blue-600">Contact Us</Link>
                     </div>
                 </div>
             )}
