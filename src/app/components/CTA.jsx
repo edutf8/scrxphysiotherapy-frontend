3b12558744c42e14db8f2da10f46bd0c15bb54b2
Edward Lewis <e.lewis371@gmail.com>
2024-08-24 16:07:52 +0100

    Update CTA design

--- src/app/components/CTA.jsx
+++ src/app/components/CTA.jsx
@@ -5,10 +5,12 @@ export default function CTA() {
     return (
         <section className="bg-blue-600 text-white py-6 md:py-8">
             <div className="container mx-auto px-4 text-center">
-                <h2 className="text-2xl md:text-3xl font-semibold mb-4">Ready to Experience the Difference?</h2>
-                <p className="text-lg mb-8">
-                    Book your appointment now and start your journey to better health and well-being.
-                </p>
+                <p className="text-lg">South Coast Rx Physiotherapy<br/>We keep you doing the things you love</p>
+                <h1 className="text-2xl md:text-3xl font-semibold mt-4 mb-4 uppercase">
+                    Take your next step with us
+                    <br/>
+                    Find Out How We Can Help You.
+                </h1>
                 <Link href="/https://south-coast-rx-physiotherapy12.selectandbook.com/">
                     <button
                         className="bg-white text-blue-600 font-semibold px-6 py-3 rounded-full shadow-lg hover:bg-gray-100 transition duration-300">
