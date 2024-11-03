debeae2f3e29dc157c042368a4f1d8d60f5041a2
Edward Lewis <e.lewis371@gmail.com>
2024-11-03 22:42:56 +0000

    Correction

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,17 +2,24 @@ export default function Header() {
     return (
         <div className={"bg-white border-b-2 border-white"}>
             <div className={"max-w-7xl px-4 py-2 sm:px-6 lg:px-8 mx-auto"}>
-                <div className={"grid justify-center md:grid-cols-2 md:justify-between md:items-center gap-2"}>
-                    <div className={"text-center md:text-start flex items-center gap-2"}>
+                <div className={"flex justify-between items-center"}>
+
+                    {/* Left Text Section */}
+                    <div className={"text-left"}>
                         <p className={"mt-1 text-blue-600 font-medium"}>Portsmouth&#39;s Highest Rated Physiotherapy Clinic</p>
+                    </div>
+
+                    {/* Center Google Review Image */}
+                    <div className={"flex justify-center"}>
                         <img
                             src="/googlereview.png"
                             alt="Google 5-star rating"
-                            className="w-18 h-10 align-middle" // Adjust the size as needed
+                            className="w-18 h-10" // Adjust size as needed
                         />
                     </div>
 
-                    <div className={"mt-1 text-center md:text-start md:flex md:justify-end md:items-center"}>
+                    {/* Right Book Button */}
+                    <div className={"text-right"}>
                         <a className={"py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium" +
                             " rounded-full border border-gray-200 bg-blue-500 text-gray-800 shadow-sm hover:bg-blue-700" +
                             " focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none text-white"}
@@ -20,6 +27,7 @@ export default function Header() {
                             Book Today
                         </a>
                     </div>
+
                 </div>
             </div>
         </div>
