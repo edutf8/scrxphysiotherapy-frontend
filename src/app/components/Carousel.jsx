9f1aa9cfa6bb062426c8ba92d03fc40f71a66736
Edward Lewis <e.lewis371@gmail.com>
2024-11-03 21:25:59 +0000

    Change background

--- src/app/components/Carousel.jsx
+++ src/app/components/Carousel.jsx
@@ -68,7 +68,7 @@ export const Carousel = ({
         <div
             ref={containerRef}
             className={cn(
-                "scroller relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+                "scroller background-gray relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
                 className
             )}
         >
