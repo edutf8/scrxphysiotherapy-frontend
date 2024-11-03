bffac6144c232f8d2d3db772509a558d4b9a782c
Edward Lewis <e.lewis371@gmail.com>
2024-11-03 22:29:03 +0000

    change literally everything

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header() {
     return (
-        <div className={"bg-gradient-to-r from-blue-500 via-blue-700 to-blue-500 border-b-2 border-white"}>
+        <div className={"bg-white border-b-2 border-white"}>
             <div className={"max-w-[85rem] px-4 py-2 sm:px-6 lg:px-8 mx-auto"}>
                 <div className={"grid justify-center md:grid-cols-2 md:justify-between md:items-center gap-2"}>
                     <div className={"text-center md:text-start flex items-center gap-2"}>
-                        <p className={"mt-1 text-white font-medium"}>Portsmouth&#39;s Highest Rated Physiotherapy Clinic</p>
+                        <p className={"mt-1 text-blue-600 font-medium"}>Portsmouth&#39;s Highest Rated Physiotherapy Clinic</p>
                         <img
                             src="/googlereview.png"
                             alt="Google 5-star rating"
@@ -14,8 +14,8 @@ export default function Header() {
 
                     <div className={"mt-1 text-center md:text-start md:flex md:justify-end md:items-center"}>
                         <a className={"py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium" +
-                            " rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50" +
-                            " focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none "}
+                            " rounded-full border border-gray-200 bg-blue-500 text-gray-800 shadow-sm hover:bg-blue-700" +
+                            " focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none text-white"}
                            href={"https://south-coast-rx-physiotherapy12.selectandbook.com/"}>
                             Book Today
                         </a>
