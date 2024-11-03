42a89f71372d9f22554055124ff784dad45875de
Edward Lewis <e.lewis371@gmail.com>
2024-11-03 22:47:10 +0000

    Another change

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,13 +4,8 @@ export default function Header() {
             <div className={"max-w-7xl px-4 py-2 sm:px-6 lg:px-8 mx-auto"}>
                 <div className={"flex justify-between items-center"}>
 
-                    {/* Left Text Section */}
-                    <div className={"text-left"}>
-                        <p className={"mt-1 text-blue-600 font-medium"}>Portsmouth&#39;s Highest Rated Physiotherapy Clinic</p>
-                    </div>
-
-                    {/* Center Google Review Image */}
-                    <div className={"flex justify-center"}>
+                    <div className={"flex items-center gap-2"}>
+                        <p className={"text-blue-600 font-medium"}>Portsmouth&#39;s Highest Rated Physiotherapy Clinic</p>
                         <img
                             src="/googlereview.png"
                             alt="Google 5-star rating"
@@ -18,11 +13,10 @@ export default function Header() {
                         />
                     </div>
 
-                    {/* Right Book Button */}
-                    <div className={"text-right"}>
+                    <div>
                         <a className={"py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium" +
-                            " rounded-full border border-gray-200 bg-blue-500 text-gray-800 shadow-sm hover:bg-blue-700" +
-                            " focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none text-white"}
+                            " rounded-full border border-gray-200 bg-blue-500 text-white shadow-sm hover:bg-blue-700" +
+                            " focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"}
                            href={"https://south-coast-rx-physiotherapy12.selectandbook.com/"}>
                             Book Today
                         </a>
