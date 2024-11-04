9b5ae3eb379b6e5238f7f21306ca8a6d96150d09
Edward Lewis <e.lewis371@gmail.com>
2024-11-04 13:58:01 +0000

    Purpose

--- src/app/vision/page.jsx
+++ src/app/vision/page.jsx
@@ -32,15 +32,12 @@ export default function Vision() {
                     <h1 className="text-3xl md:text-5xl font-bold mb-4">
                         At South Coast Rx, we strive to raise the bar for Physiotherapy and health and well-being provision.
                     </h1>
-                    <p className="text-lg md:text-xl">
-                        Our purpose is to improve the lives of others.
-                    </p>
                 </div>
             </section>
 
             <section className="py-12 md:py-16 bg-gray-100">
                 <div className="container mx-auto px-4">
-                    <h2 className="text-2xl md:text-3xl font-semibold text-center mb-8 md:mb-12">Our Core Values</h2>
+                    <h2 className="text-2xl md:text-3xl font-semibold text-center mb-8 md:mb-12">Our Vision & Values</h2>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
                         <div className="bg-white p-4 md:p-6 rounded-lg shadow-lg transform hover:scale-105 transition-transform duration-300">
                             <div className="flex items-center justify-center mb-4">
@@ -81,15 +78,28 @@ export default function Vision() {
                     </div>
                 </div>
             </section>
-
+            <section id={"our-purpose"}>
+                <div className={"container mx-auto px-4 mt-10"}>
+                    <h2 className="text-2xl md:text-3xl font-semibold text-center mb-8 md:mb-12">Our Purpose</h2>
+                    <div
+                        className="bg-white p-4 md:p-6 rounded-lg shadow-lg transform hover:scale-105 transition-transform duration-300">
+                        <div className="flex items-center justify-center mb-4">
+                            <FaHandsHelping className="text-blue-600 w-10 h-10 md:w-12 md:h-12" />
+                        </div>
+                        <p className="text-gray-700 text-center">
+                            To improve the lives of others
+                        </p>
+                    </div>
+                </div>
+            </section>
             <section id={"call-to-action"}>
                 <div className={"mx-auto mt-5"}>
-                    <CTA />
+                    <CTA/>
                 </div>
             </section>
 
             <section id="footer">
-                <Footer />
+                <Footer/>
             </section>
         </div>
     );
