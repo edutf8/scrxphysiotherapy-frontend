ce983e2ccb1e9e395e32553d52c6adfc5ba3d7dd
Edward Lewis <e.lewis371@gmail.com>
2024-11-14 15:32:52 +0000

    center stuff

--- src/app/components/PartnersGrid.jsx
+++ src/app/components/PartnersGrid.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const PartnersGrid = ({ partners }) => {
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-4 p-4">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-4 p-4 justify-items-center">
             {partners.map((partner, index) => (
                 <div
                     key={index}
@@ -24,3 +24,4 @@ const PartnersGrid = ({ partners }) => {
 };
 
 export default PartnersGrid;
+
