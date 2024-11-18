53743cab962b83a38e20580e2a98953fe015426f
Edward Lewis <e.lewis371@gmail.com>
2024-11-18 12:01:24 +0000

    fix

--- src/app/components/TeamLayout.jsx
+++ src/app/components/TeamLayout.jsx
@@ -55,7 +55,7 @@ const fetchData = async () => {
             throw new Error(`Network response was not ok, status: ${response.status}`);
         }
 
-        console.log(response);
+        console.log(response.text());
         const data = await response.json();
         console.log(data);
         return Array.isArray(data) ? data : [data];
